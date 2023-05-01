c853475c829eb8ee959af26f4d815986b536c087
Albert <39496218+albertturon1@users.noreply.github.com>
2023-05-01 14:56:17 +0200

    fix: made bigger NavbarMenuButon lines

--- src/components/navbar/NavbarMenuButton.tsx
+++ src/components/navbar/NavbarMenuButton.tsx
@@ -11,12 +11,12 @@ const NavbarMenuButton = ({
     className="focus:ring-ring flex flex-col items-center justify-center rounded-md p-2 py-4 focus:outline-none focus:ring-2 focus:ring-inset sm:hidden"
   >
     <div
-      className={`h-[0.4px] w-6 bg-navy transition duration-300 ${
+      className={`h-[0.7px] w-6 bg-navy transition duration-300 ${
         open ? 'translate-y-[1px] rotate-45' : '-translate-y-1'
       }`}
     />
     <div
-      className={`h-[0.4px] w-6 bg-navy transition duration-300 ${
+      className={`h-[0.7px] w-6 bg-navy transition duration-300 ${
         open ? '-rotate-45' : 'translate-y-1'
       }`}
     />
